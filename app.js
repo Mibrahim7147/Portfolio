f88f392a21289e493d93959cba5512a1f94589b7
Mohamed Ibrahim <mibrahim7147@gmail.com>
2025-03-12 04:55:19 -0400

    updated styling and new logo

--- app.js
+++ app.js
@@ -15,6 +15,25 @@ function PageTransitions() {
             this.classList.add('active-btn'); // FIXED
         });
     }
+
+    //section active class
+    allsections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected 
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hides other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions();
